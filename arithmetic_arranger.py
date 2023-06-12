bc39e3a7c5ba1c4a345792f772313d4de424b72e
sam88mohd <sam88mohd@gmail.com>
2023-06-12 23:24:49 +0800

    2nd commit

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -6,8 +6,23 @@ def arithmetic_arranger(problems, result=False):
       "-": operator.sub
   }
 
+  top = []
+  bottom = []
+  lines = []
+
   for problem in problems:
     chunks = problem.split()
-    print(chunks)
+    max_len = len(chunks)
+    line_len = max_len + 2
+
+    first_num = chunks[0].rjust(line_len)
+    second_num = f"{chunks[1]}{' ' * (line_len - len(chunks[2]) - 1)}{chunks[2]}"
+    line = "-" * line_len
+
+    top.append(first_num)
+    bottom.append(second_num)
+    lines.append(line)
 
     
+    
+  print(top, bottom, lines)
