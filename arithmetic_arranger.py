27fca17b3cdcad0fc6144e81822fd4b92ec629fe
sam88mohd <sam88mohd@gmail.com>
2023-06-14 00:26:16 +0800

    3rd commit

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -9,6 +9,7 @@ def arithmetic_arranger(problems, result=False):
   top = []
   bottom = []
   lines = []
+  results = []
 
   for problem in problems:
     chunks = problem.split()
@@ -22,7 +23,15 @@ def arithmetic_arranger(problems, result=False):
     top.append(first_num)
     bottom.append(second_num)
     lines.append(line)
+    
+    arranged_problems = "\n".join(["    ".join(chunk) for chunk in (top, bottom, lines)])
+    
+    if result:
+      r = opts[chunks[1]](int(chunks[0]), int(chunks[2]))
+      results.append(r)
 
-    
-    
-  print(top, bottom, lines)
+      arranged_problems += "\n" + "    ".join([" ".join(str(result)) for result in results])
+  return arranged_problems
+
+print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"], True))
+
