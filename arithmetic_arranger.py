dd57dc320dbf83d6c05787b3b83d4ee6a98ccc39
sam88mohd <sam88mohd@gmail.com>
2023-06-16 20:09:26 +0800

    completed solution

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -11,10 +11,20 @@ def arithmetic_arranger(problems, result=False):
   lines = []
   results = []
 
+  if len(problems) > 5:
+    return "Error: Too many problems."
+
   for problem in problems:
     chunks = problem.split()
-    max_len = len(chunks)
+    max_len = max([len(chunk) for chunk in chunks])
     line_len = max_len + 2
+
+    if max_len > 4:
+      return "Error: Numbers cannot be more than four digits."
+    elif chunks[1] not in opts.keys():
+      return "Error: Operator must be '+' or '-'."
+    elif not all([chunk.isnumeric() for chunk in chunks[::2]]):
+      return "Error: Numbers must only contain digits."
 
     first_num = chunks[0].rjust(line_len)
     second_num = f"{chunks[1]}{' ' * (line_len - len(chunks[2]) - 1)}{chunks[2]}"
@@ -27,11 +37,10 @@ def arithmetic_arranger(problems, result=False):
     arranged_problems = "\n".join(["    ".join(chunk) for chunk in (top, bottom, lines)])
     
     if result:
-      r = opts[chunks[1]](int(chunks[0]), int(chunks[2]))
-      results.append(r)
+      calc = opts[chunks[1]](int(chunks[0]), int(chunks[2]))
+      results.append(f"{str(calc).rjust(line_len)}")
 
-      arranged_problems += "\n" + "    ".join([" ".join(str(result)) for result in results])
+      arranged_problems += "\n" + "    ".join(results)
   return arranged_problems
 
-print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"], True))
 
--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from pytest import main
 from arithmetic_arranger import arithmetic_arranger
 
 
-print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"], True))
+print(arithmetic_arranger(['32 - 698', '1 - 3801', '45 + 43', '123 + 49', '988 + 40'], True))
 
 
 # Run unit tests automatically
-# main(['-vv'])
+main(['-vv'])
